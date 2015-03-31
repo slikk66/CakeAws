f0575346e22b0e4834968bcb89d7d23abda46167
Dan Billeci <dbilleci@mediatemple.net>
2015-03-31 11:54:39 -0700

    tweak scripts

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-
-HTTPDUSER=`ps aux | grep -E '[a]pache|[h]ttpd|[_]www|[w]ww-data|[n]ginx' | grep -v root | head -1 | cut -d\  -f1`
-setfacl -R -m u:${HTTPDUSER}:rwx /var/www/html/CakeAws/app/tmp
-setfacl -R -d -m u:${HTTPDUSER}:rwx /var/www/html/CakeAws/app/tmp
-
-service httpd start
+chown -R apache:apache /var/www/html/CakeAws/app/tmp
+chmod -R 755 /var/www/html/CakeAws/app/tmp
+chmod -R 755 /var/www/html/CakeAws/app/tmp
--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-service httpd stop
 rm -f /etc/httpd/conf.d/cakeaws.com.conf || true
